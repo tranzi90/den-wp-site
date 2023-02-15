74f73b1bd26e386df0813c3a487f46efa1c39c1c
denis_levchenko <tranzi90@gmail.com>
2023-02-15 21:32:53 +0300

    refactor to use template parts

--- app/public/wp-content/themes/fictional-university/archive-program.php
+++ app/public/wp-content/themes/fictional-university/archive-program.php
@@ -1,16 +1,10 @@
-<?php get_header(); ?>
+<?php get_header();
 
-    <div class="page-banner">
-        <div class="page-banner__bg-image"
-             style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
-        <div class="page-banner__content container container--narrow">
-            <h1 class="page-banner__title">All Programs</h1>
-            <div class="page-banner__intro">
-                <p>Our wizard learning courses</p>
-            </div>
-        </div>
-    </div>
-
+    pageBanner(array(
+        'title' => 'All Programs',
+        'subtitle' => 'Our wizard learning courses'
+    ));
+?>
     <div class="container container--narrow page-section">
         <ul class="link-list min-list">
         <?php
--- app/public/wp-content/themes/fictional-university/single-program.php
+++ app/public/wp-content/themes/fictional-university/single-program.php
@@ -2,18 +2,10 @@
 get_header();
 
 while (have_posts()) {
-    the_post(); ?>
+    the_post();
 
-    <div class="page-banner">
-        <div class="page-banner__bg-image"
-             style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
-        <div class="page-banner__content container container--narrow">
-            <h1 class="page-banner__title"><?php the_title(); ?></h1>
-            <div class="page-banner__intro">
-                <p>Learn how the school of your dreams got started.</p>
-            </div>
-        </div>
-    </div>
+    pageBanner();
+    ?>
 
     <div class="container container--narrow page-section">
         <div class="metabox metabox--position-up metabox--with-home-link">
@@ -94,26 +86,10 @@ while (have_posts()) {
                 <h2 class="headline headline--medium">Upcoming <?php the_title() ?> Events</h2>
             <?php
                 while ($relatedEvents->have_posts()) {
-                    $relatedEvents->the_post(); ?>
+                    $relatedEvents->the_post();
 
-                    <div class="event-summary">
-                        <a class="event-summary__date t-center" href="#">
-                                <span class="event-summary__month"><?php
-                                    $eventDate = new DateTime(get_field('event_date'));
-                                    echo $eventDate->format('M');
-                                    ?></span>
-                            <span class="event-summary__day"><?php echo $eventDate->format('d') ?></span>
-                        </a>
-                        <div class="event-summary__content">
-                            <h5 class="event-summary__title headline headline--tiny">
-                                <a href="<?php the_permalink(); ?>"><?php the_title() ?></a>
-                            </h5>
-                            <p><?php echo wp_trim_words(get_the_content(), 18)?>
-                                <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a>
-                            </p>
-                        </div>
-                    </div>
-                <?php }
+                    get_template_part('template-parts/event');
+                }
             }
         ?>
     </div>
